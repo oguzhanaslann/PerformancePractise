d6e54ba586d09c0f18c2aed82e9c1ecc95470da0
oguzhanaslann <oguz21199@gmail.com>
2022-07-05 18:26:09 +0300

    - appStandbyBucket addedf

--- app/src/main/java/com/oguzhanaslann/performancepractise/App.kt
+++ app/src/main/java/com/oguzhanaslann/performancepractise/App.kt
@@ -1,12 +1,21 @@
 package com.oguzhanaslann.performancepractise
 
 import android.app.Application
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import android.os.Build
 import android.os.StrictMode
+import android.util.Log
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+            Log.d("TAG", "onCreate: ${usageStatsManager.appStandbyBucket}" )
+        }
 
         StrictMode.setVmPolicy(
             StrictMode.VmPolicy
